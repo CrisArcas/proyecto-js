4509874b14b80f182c929753e92b766621343ae2
cristian arcas <cristianarcas1@gmail.com>
2023-11-28 14:16:46 -0300

    Creación de HTML, arrayProductos

--- js/main.js
+++ js/main.js
@@ -80,3 +80,43 @@ if(total >= 60000){
 alert("El Total de compra es " + "$" +total);
 alert("Gracias por su compra!");
 
+class Producto {
+    constructor(nombre, precio, img){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.img = img;
+    }
+}
+
+const PRODUCTO_1 = new Producto("Zapatillas Nano X3", 77560, "./assets/img/nano1.jpg");
+const PRODUCTO_2 = new Producto("Zapatillas Nano X3", 77560, "./assets/img/nano1.jpg");
+const PRODUCTO_3 = new Producto("Zapatillas Nano X3", 77560, "./assets/img/nano1.jpg");
+const PRODUCTO_4 = new Producto("Zapatillas Nano X3", 77560, "./assets/img/nano1.jpg");
+const PRODUCTO_5 = new Producto("Zapatillas Nano X3", 77560, "./assets/img/nano1.jpg");
+const PRODUCTO_6 = new Producto("Zapatillas Nano X3", 77560, "./assets/img/nano1.jpg");
+const PRODUCTO_7 = new Producto("Zapatillas Nano X3", 77560, "./assets/img/nano1.jpg");
+const PRODUCTO_8 = new Producto("Zapatillas Nano X3", 77560, "./assets/img/nano1.jpg");
+const PRODUCTO_9 = new Producto("Zapatillas Nano X3", 77560, "./assets/img/nano1.jpg");
+const PRODUCTO_10 = new Producto("Zapatillas Nano X3", 77560, "./assets/img/nano1.jpg");
+const PRODUCTO_11 = new Producto("Zapatillas Nano X3", 77560, "./assets/img/nano1.jpg");
+const PRODUCTO_12 = new Producto("Zapatillas Nano X3", 77560, "./assets/img/nano1.jpg");
+
+const ArrayProductos = [PRODUCTO_1, PRODUCTO_2, PRODUCTO_3, PRODUCTO_4, PRODUCTO_5, PRODUCTO_6, PRODUCTO_7, PRODUCTO_8, PRODUCTO_9, PRODUCTO_10, PRODUCTO_11, PRODUCTO_12,];
+
+ArrayProductos.forEach(producto => {
+    let section = document.createElement("div");
+    section.className = "card";
+    section.innerHTML = `
+                    <div class="card bg-dark align-items-center border-0" style="width: 300px;">
+                            <img src="${producto.img}" class="card-img-top " alt="...">
+                        <div class="card-body alig-items-center">
+                            <h5 class="card-title text-light text-center">${producto.nombre}</h5>
+                            <h5 class="card-text text-light text-center">$ ${producto.precio}</h5>
+                            <a href="#" class="btn btn-primary mx-auto d-block">COMPRAR</a>
+                        </div>
+                    </div>
+`
+    sectionProductos.appendChild(section);
+    }
+)
+
